c797403d4c0f731d956c7d841fe4e4d34044f74f
vindhya choudary <vindhyachoudary.dev@gmail.com>
2026-01-31 17:18:24 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,5 +59,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
 
     // Activity Recognition (for fall detection context)
-    implementation("com.google.android.gms:play-services-activity-recognition:21.0.0")
+    implementation("com.google.android.gms:play-services-activity-recognition:20.0.0")
 }
+
